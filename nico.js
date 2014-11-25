dfb916ed64bf873cc5a90bafc11df9062524f900
nansir <qianduan51@qq.com>
2014-11-25 11:20:24 +0800

    xlib不生成文档

--- nico.js
+++ nico.js
@@ -4,32 +4,36 @@ exports.package = pkg;
 
 // {{ settings for nico
 exports.theme = __dirname
-console.log('theme:',__dirname)
+console.log('theme:', __dirname)
 exports.source = process.cwd()
 exports.output = path.join(process.cwd(), '_site')
 exports.permalink = '{{directory}}/{{filename}}.html'
-exports.ignorefilter = function(filepath, subdir) {
-  var extname = path.extname(filepath);
-  if (extname === '.tmp' || extname === '.bak') {
-    return false;
-  }
-  if (/\.DS_Store/.test(filepath)) {
-    return false;
-  }
-  if (/^sea-modules/.test(subdir) &&
-      /\.[md|html|psd|zip|yml]/.test(path.extname(filepath))) {
-    return false;
-  }
-  if (/^(_site|_theme|node_modules|\.idea)/.test(subdir)) {
-    return false;
-  }
-  return true;
+exports.ignorefilter = function (filepath, subdir) {
+    var extname = path.extname(filepath);
+    if (extname === '.tmp' || extname === '.bak') {
+        return false;
+    }
+    if (/\.DS_Store/.test(filepath)) {
+        return false;
+    }
+    if (/^sea-modules/.test(subdir) &&
+        /\.[md|html|psd|zip|yml]/.test(path.extname(filepath))) {
+        return false;
+    }
+    if (/^xdoc/.test(subdir) &&
+        /\.[md|html|psd|zip|yml]/.test(path.extname(filepath))) {
+        return false;
+    }
+    if (/^(_site|_theme|node_modules|\.idea)/.test(subdir)) {
+        return false;
+    }
+    return true;
 }
 exports.writers = [
-  'nico.PageWriter',
-  'nico.StaticWriter',
-  'nico.FileWriter',
-  'nico.MochaWriter'
+    'nico.PageWriter',
+    'nico.StaticWriter',
+    'nico.FileWriter',
+    'nico.MochaWriter'
 ]
 // end settings }}
 
@@ -39,20 +43,20 @@ exports.assets_host = 'http://assets.spmjs.org';
 
 exports.filters = {}
 
-exports.isCssModule = (function() {
- // 名称若恰好为 stylib
-  if (pkg.family === 'alice' || pkg.name === 'stylib') {
-    return true
-  }
-  // output 中全是样式才用 alice
-  var output = pkg.spm.output
-  if (output) {
-    for (var i in output) {
-      var f = output[i]
-      if (!/\.(css|stylus|less)$/.test(f)) return false
+exports.isCssModule = (function () {
+    // 名称若恰好为 stylib
+    if (pkg.family === 'alice' || pkg.name === 'stylib') {
+        return true
     }
-  } else {
+    // output 中全是样式才用 alice
+    var output = pkg.spm.output
+    if (output) {
+        for (var i in output) {
+            var f = output[i]
+            if (!/\.(css|stylus|less)$/.test(f)) return false
+        }
+    } else {
+        return true
+    }
     return true
-  }
-  return true
 })()
